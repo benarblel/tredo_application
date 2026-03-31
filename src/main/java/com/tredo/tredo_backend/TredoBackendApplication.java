97601e0e811a45ee7a8daf7903eb959961614d7c
benarblel <lucas.benarble69@gmail.com>
2026-03-31 16:53:55 +0200

    Premier commit

---
+++ src/main/java/com/tredo/tredo_backend/TredoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tredo.tredo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TredoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TredoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tredo/tredo_backend/TredoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tredo.tredo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TredoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
