ebe15cd36435227beda764d593bec0abea7356c1
lucas_dev <lucas.benarble69@gmail.com>
2026-04-20 15:25:26 +0200

    Création des tables dans le fichier domain

--- src/main/java/com/tredo/tredo_backend/domain/Card.java
+++ src/main/java/com/tredo/tredo_backend/domain/Card.java
@@ -1,7 +1,19 @@
 package com.tredo.tredo_backend.domain;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
 public class Card {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private int position;
+
+    @ManyToOne @JoinColumn(name = "list_id") @JsonBackReference
+    private CardList cardList;
 }
