86777705c029135d8addf2dfba6f48331672da1b
lucas_dev <lucas.benarble69@gmail.com>
2026-04-22 11:54:20 +0200

    Correction du repository Card

--- src/main/java/com/tredo/tredo_backend/repository/CardRepository.java
+++ src/main/java/com/tredo/tredo_backend/repository/CardRepository.java
@@ -2,9 +2,11 @@ package com.tredo.tredo_backend.repository;
 
 import com.tredo.tredo_backend.domain.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
     List<Card> findByCardListId(Long cardListId);
 }
