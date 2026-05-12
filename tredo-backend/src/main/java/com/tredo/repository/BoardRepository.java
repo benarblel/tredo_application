6a2c7d6701194ae072020203b52390c4aa74e744
lucas_dev <lucas.benarble69@gmail.com>
2026-05-12 12:21:42 +0200

    Suppression tredo-backend

--- tredo-backend/src/main/java/com/tredo/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tredo.repository;
-
-import com.tredo.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
